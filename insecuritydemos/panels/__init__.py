6d8b87f14fdab6cb72a32397eac27529c4e9a21a
Double Green Vole <double.green.vole@gmail.com>
2013-12-29 21:34:56 -0800

    Removed dead code.

--- insecuritydemos/panels/__init__.py
+++
@@ -1,7 +0,0 @@
-from keylogger import *
-from packetsniffer import *
-from passwordcracker import *
-from mdnscapture import *
-from mdnsmon0capture import *
-from httpcapture import *
-from wlanprobes import *
