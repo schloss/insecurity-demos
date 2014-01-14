232759e995c33bb5f22a98b2435c2394bf1d549a
Double Green Vole <double.green.vole@gmail.com>
2014-01-14 00:16:55 -0800

    Reinstated current network assignment using TShark, but only BSSIDs listed.

--- insecuritydemos/demos/wireless_demos.py
+++ insecuritydemos/demos/wireless_demos.py
@@ -3,17 +3,21 @@ class AccessPointDemo():
     MONITOR_MODE = True
     WIRELESS_NETWORKS_CONTROL = False
     TSHARK_FIELDS = ('wlan.sa',
-                     'wlan_mgt.ssid')
+                     'wlan_mgt.ssid',
+                     'wlan.bssid')
     TSHARK_READ_FILTER = None
-    TSHARK_CAPTURE_FILTER = 'subtype probereq'
+    TSHARK_CAPTURE_FILTER = 'subtype probereq or type data'
 
     def interpret_tshark_output(self, fields):
         out = {}
-        if fields and len(fields) == 2 and fields[0]:
+        if fields and len(fields) == 3 and fields[0]:
             out['mac'] = fields[0]
             probe = fields[1]
             if probe:
                 out['aps'] = [{'essid' : probe}]
+            ap = fields[2]
+            if ap and ap != 'ff:ff:ff:ff:ff:ff':
+                out['current_network'] = {'bssid' : ap}
         return out
 
 class HttpBasicAuthSniffDemo():
